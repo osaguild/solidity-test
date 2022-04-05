6d2f776db694d995575e2271a059b487e6dd9c22
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-05 11:39:32 +0900

    add selfdestruct

--- src/Faucet.sol
+++ src/Faucet.sol
@@ -5,9 +5,22 @@ pragma solidity ^0.6.4;
 
 // Our first contract is a faucet!
 contract Faucet {
+    address payable owner;
+
+    // Contract constructor: set owner
+    constructor() public {
+        owner = msg.sender;
+    }
+
     // Accept any incoming amount
     receive() external payable {}
 
+    // Contract destructor
+    function destroy() public {
+        require(msg.sender == owner);
+        selfdestruct(owner);
+    }
+    
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
         // Limit withdrawal amount
