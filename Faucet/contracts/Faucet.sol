5ee8249b4068da2d6867150c379d8bb9c24f54d1
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-07 18:11:52 +0900

    fix

--- Faucet/contracts/Faucet.sol
+++ Faucet/contracts/Faucet.sol
@@ -3,7 +3,8 @@
 // Version of Solidity compiler this program was written for
 pragma solidity ^0.6.4;
 
-contract Owned {
+// Our first contract is a faucet!
+contract Faucet {
     address payable owner;
 
     // Contract constructor: set owner
@@ -13,40 +14,24 @@ contract Owned {
 
     // Access control modifier
     modifier onlyOwner {
-        require(msg.sender == owner, "Only the contract owner can call this function");
+        require(msg.sender == owner);
         _;
     }
-}
 
-contract Mortal is Owned {
+    // Accept any incoming amount
+    receive() external payable {}
+
     // Contract destructor
     function destroy() public onlyOwner {
         selfdestruct(owner);
-    }
-}
-
-contract Faucet is Mortal {
-    event Withdrawal(address indexed to, uint amount);
-    event Deposit(address indexed from, uint amount);
-    
-    // Accept any incoming amount
-    receive() external payable {
-        emit Deposit(msg.sender, msg.value);
     }
 
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
         // Limit withdrawal amount
-        require(withdraw_amount <= 0.1 ether);
-
-        require(
-            address(this).balance >= withdraw_amount,
-            "Insufficient balance in faucet for withdrawal request"
-        );
+        require(withdraw_amount <= 100000000000000000);
 
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
-
-        emit Withdrawal(msg.sender, withdraw_amount);
     }
 }
