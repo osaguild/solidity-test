13ed6fed3d233aa20dc4e7fb1c1b6d423dbe14e1
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-07 19:26:19 +0900

    fix

--- Faucet/test/faucet.js
+++ Faucet/test/faucet.js
@@ -8,20 +8,33 @@ contract('Faucet', (accounts) => {
   });
   it('check withdraw', async () => {
     const faucetInstance = await Faucet.deployed();
-    var withdraw_tx;
 
     // send 1 eth to contract
+    var from_1;
     await faucetInstance.send(web3.utils.toWei('1', 'ether'))
-    .then(res => {
-      console.log('send tx: ', res.tx);
-    })
+      .then(res => {
+        from_1 = res.receipt.from;
+      })
+
+    // check balance
+    await web3.eth.getBalance(from_1)
+      .then(res => {
+        console.log(res);
+      })
 
     // withdraw 0.1 eth from contract
+    var from_2;
     await faucetInstance.withdraw(web3.utils.toWei('0.1', "ether"))
-    .then(res => {
-      console.log('withdraw tx: ', res.tx);
-    })
-    
+      .then(res => {
+        from_2 = res.receipt.from;
+      })
+
+    // check balance
+    await web3.eth.getBalance(from_2)
+      .then(res => {
+        console.log(res);
+      })
+
     assert.ok(true);
   });
 });
