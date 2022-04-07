66c288961558f4c9e3d67e75fe68ce099e06f7cd
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-07 21:55:05 +0900

    fix test

--- Faucet/test/faucet.js
+++ Faucet/test/faucet.js
@@ -10,31 +10,58 @@ contract('Faucet', (accounts) => {
     const faucetInstance = await Faucet.deployed();
 
     // send 1 eth to contract
-    var from_1;
+    var r_b;
     await faucetInstance.send(web3.utils.toWei('1', 'ether'))
       .then(res => {
-        from_1 = res.receipt.from;
+        r_b = res.receipt;
       })
 
     // check balance
-    await web3.eth.getBalance(from_1)
+    var r_b_a;
+    var r_b_c;
+    await web3.eth.getBalance(r_b.from)
       .then(res => {
-        console.log(res);
+        r_b_a = res;
+      })
+    await web3.eth.getBalance(r_b.to)
+      .then(res => {
+        r_b_c = res;
       })
 
     // withdraw 0.1 eth from contract
-    var from_2;
+    var r_a;
     await faucetInstance.withdraw(web3.utils.toWei('0.1', "ether"))
       .then(res => {
-        from_2 = res.receipt.from;
+        r_a = res.receipt;
       })
 
     // check balance
-    await web3.eth.getBalance(from_2)
+    var r_a_a;
+    var r_a_c;
+    await web3.eth.getBalance(r_a.from)
       .then(res => {
-        console.log(res);
+        r_a_a = res;
+      })
+    await web3.eth.getBalance(r_a.to)
+      .then(res => {
+        r_a_c = res;
       })
 
+    console.log('      account balance  ', r_b_a, ' -> ', r_a_a, '(', r_b_a - r_a_a, ')');
+    console.log('      contract balance ', r_b_c, ' -> ', r_a_c, '(', r_b_c - r_a_c, ')');
+
     assert.ok(true);
+  });
+  it('check destroy', async () => {
+    const faucetInstance = await Faucet.deployed();
+
+    // check destroy
+    var destroy;
+    await faucetInstance.destroy()
+      .then(res => {
+        destroy = res;
+      })
+
+    assert(destroy, 'miss');
   });
 });
--- Faucet/test/faucet2.js
+++ Faucet/test/faucet2.js
@@ -1,9 +1,67 @@
-const Faucet2 = artifacts.require("Faucet2");
+const Faucet = artifacts.require("Faucet2");
 
 contract('Faucet2', (accounts) => {
   it('check deploy', async () => {
-    const faucet2Instance = await Faucet2.deployed();
+    const faucetInstance = await Faucet.deployed();
 
-    assert(faucet2Instance, 'miss');
+    assert(faucetInstance, 'miss');
+  });
+  it('check withdraw', async () => {
+    const faucetInstance = await Faucet.deployed();
+
+    // send 1 eth to contract
+    var r_b;
+    await faucetInstance.send(web3.utils.toWei('1', 'ether'))
+      .then(res => {
+        r_b = res.receipt;
+      })
+
+    // check balance
+    var r_b_a;
+    var r_b_c;
+    await web3.eth.getBalance(r_b.from)
+      .then(res => {
+        r_b_a = res;
+      })
+    await web3.eth.getBalance(r_b.to)
+      .then(res => {
+        r_b_c = res;
+      })
+
+    // withdraw 0.1 eth from contract
+    var r_a;
+    await faucetInstance.withdraw(web3.utils.toWei('0.1', "ether"))
+      .then(res => {
+        r_a = res.receipt;
+      })
+
+    // check balance
+    var r_a_a;
+    var r_a_c;
+    await web3.eth.getBalance(r_a.from)
+      .then(res => {
+        r_a_a = res;
+      })
+    await web3.eth.getBalance(r_a.to)
+      .then(res => {
+        r_a_c = res;
+      })
+
+    console.log('      account balance  ', r_b_a, ' -> ', r_a_a, '(', r_b_a - r_a_a, ')');
+    console.log('      contract balance ', r_b_c, ' -> ', r_a_c, '(', r_b_c - r_a_c, ')');
+
+    assert.ok(true);
+  });
+  it('check destroy', async () => {
+    const faucetInstance = await Faucet.deployed();
+
+    // check destroy
+    var destroy;
+    await faucetInstance.destroy()
+      .then(res => {
+        destroy = res;
+      })
+
+    assert(destroy, 'miss');
   });
 });
