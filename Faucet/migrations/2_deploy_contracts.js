d828ff72bfad150dd94106512cf4e16641cc8c7f
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-07 17:59:37 +0900

    fix

--- Faucet/migrations/2_deploy_contracts.js
+++ Faucet/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Faucet = artifacts.require("Faucet");
+const Faucet2 = artifacts.require("Faucet2");
 
 module.exports = function (deployer) {
   deployer.deploy(Faucet);
+  deployer.deploy(Faucet2);
 };
--- Faucet/migrations/3_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const Faucet2 = artifacts.require("Faucet2");
-
-module.exports = function (deployer) {
-  deployer.deploy(Faucet2);
-};
