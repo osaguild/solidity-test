0e38b32480a760b12237faa6985ff3e9f8152aea
osaguild <nobuhisa.abe.0922@gmail.com>
2022-04-23 08:57:08 +0900

    fix

--- Auction/backend/migrations/2_deploy_contract.js
+++ Auction/backend/migrations/2_deploy_contract.js
@@ -2,6 +2,6 @@ const DeedRepository = artifacts.require("DeedRepository");
 const AuctionRepository = artifacts.require("AuctionRepository");
 
 module.exports = async function(deployer) {
-  await deployer.deploy(DeedRepository, "Ultra Auction NFT", "UANFT");
   await deployer.deploy(AuctionRepository);
+  await deployer.deploy(DeedRepository, "Ultra Auction NFT", "UANFT");
 };
